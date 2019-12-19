732aa7f11c17d567298e8a3838dc396a5a362682
Bastien SOARES <bsp95@hotmail.fr>
2019-12-19 15:26:02 +0100

    Add fixture with faker

--- src/DataFixtures/GaleryFixtures.php
+++ src/DataFixtures/GaleryFixtures.php
@@ -2,22 +2,38 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
+use App\Entity\Galery;
+use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
-use App\Entity\Galery;
+
 class GaleryFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        for($i = 1; $i <= 10; $i++) {
-            $galery = new Galery();
-            $galery->setTitle("Galerie n°" . $i)
-                   ->setDescription("<p>Description de la galerie</p>")
-                   ->setImage("http://placehold.it/350x150")
-                   ->setCreatedAt(new \DateTime());
+        $faker = Factory::create('fr_FR');
 
-            $manager->persist($galery);
-        }
+        // Création de catégories
+        for($i = 1; $i <= 8; $i++) {
+            $category = new Category();
+            $category->setName($faker->sentence());
+
+            $manager->persist($category);
+
+            $content = '<p>' . join($faker->paragraphs(5), '</p><p>') . '<p>';
+
+            for($j = 1; $j <= mt_rand(3, 7); $j++) {
+                $galery = new Galery();
+                $galery->setTitle($faker->sentence())
+                       ->setDescription($content)
+                       ->setImage($faker->imageUrl())
+                       ->setCreatedAt($faker->dateTimeBetween('-11 months'))
+                       ->addCategory($category);
+    
+                $manager->persist($galery);
+            }
+        } 
         $manager->flush();
     }
 }

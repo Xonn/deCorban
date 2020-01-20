6e91fcd3e46ef28d37d0960fed40bdc982cacb85
Bastien SOARES <bsp95@hotmail.fr>
2020-01-20 11:41:56 +0100

    Add ckeditor for admin

--- public/js/admin.js
+++ public/js/admin.js
@@ -8,4 +8,22 @@ $(function() {
         //inline: true,
         theme: 'dark',
     });
+
+    document.querySelectorAll( '.awesome-ckeditor textarea' )
+             .forEach(function(el){
+                 el.removeAttribute('required');
+                 ClassicEditor
+                     .create( el ,{
+                        toolbar: [ 'heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote' ],
+                     })
+                     .then( function (editor) {
+                         var div = el.parentNode.querySelector('.ck-editor__editable');
+                         //div.style.backgroundColor = 'white'; 
+                         //div.style.minHeight = '300px';
+                     } )
+                     .catch( function (error) {
+                         console.error( error );
+                     } );
+             });
+
 });

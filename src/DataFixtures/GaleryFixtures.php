04a1ec01c67757a3bc2a7af61bee2deed5c31ec0
Bastien SOARES <bsp95@hotmail.fr>
2020-01-26 22:38:57 +0100

    Update galery fixture

--- src/DataFixtures/GaleryFixtures.php
+++ src/DataFixtures/GaleryFixtures.php
@@ -17,17 +17,19 @@ class GaleryFixtures extends Fixture
         // Création de catégories
         for($i = 1; $i <= 8; $i++) {
             $category = new Category();
-            $category->setName($faker->sentence());
+            $category->setName($faker->word());
 
             $manager->persist($category);
 
             $content = '<p>' . join($faker->paragraphs(5), '</p><p>') . '<p>';
 
             for($j = 1; $j <= mt_rand(3, 7); $j++) {
+                $img = '';
+                $img = 'https://source.unsplash.com/random/1300x2000';
                 $galery = new Galery();
                 $galery->setTitle($faker->sentence())
                        ->setDescription($content)
-                       ->setThumbnail($faker->imageUrl())
+                       ->setThumbnail($img)
                        ->setCreatedAt($faker->dateTimeBetween('-11 months'))
                        ->addCategory($category);
     

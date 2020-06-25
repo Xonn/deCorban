d42ddb4d11746f52f467b8179460972c0c9d6417
Bastien SOARES <bsp95@hotmail.fr>
2020-06-25 11:39:03 +0200

    Rework contact, login, register and user profile template

--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -4,23 +4,25 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class RegistrationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('username')
-            ->add('imageFile', FileType::class, [
-                'required' => false
-            ])
-            ->add('password', PasswordType::class)
-            ->add('confirm_password', PasswordType::class)
+            ->add('email', EmailType::class, ['label' => FALSE])
+            ->add('username', TextType::class, ['label' => FALSE])
+            // ->add('imageFile', FileType::class, [
+            //     'required' => false,
+            // ])
+            ->add('password', PasswordType::class, ['label' => FALSE])
+            ->add('confirm_password', PasswordType::class, ['label' => FALSE])
         ;
     }
 

9af568b19e9cfab58e85755c0aa5737d99c56223
Bastien SOARES <bsp95@hotmail.fr>
2020-06-25 11:46:56 +0200

    Display models on homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,23 +2,27 @@
 
 namespace App\Controller;
 
+use App\Repository\ModelRepository;
 use App\Repository\GaleryRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
-     * @param GaleryRepository $repository
+     * @param GaleryRepository $galeryRepository
+     * @param ModelRepository $modelRepository
      * @return Response
      */
-    public function index(GaleryRepository $repository): Response
+    public function index(GaleryRepository $galeryRepository, ModelRepository $modelRepository): Response
     {
-        $galery = $repository->findLatest();
+        $galeries = $galeryRepository->findLatest();
+        $models = $modelRepository->findLatest();
         return $this->render('home/index.html.twig', [
-            'galeries' => $galery,
+            'galeries' => $galeries,
+            'models' => $models,
             'title' => 'Decorban',
             'subtitle' => 'L\'éromantisme',
             'last_galeries' => 'Les dernières galeries'

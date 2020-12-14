52ec01373fa907f0de6c73a411f4bde06afcb9fb
Bastien SOARES <bsp95@hotmail.fr>
2020-12-14 22:50:55 +0100

    Cleaning migration files

---
+++ src/Migrations/Version20201214214546.php
@@ -0,0 +1,85 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201214214546 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(255) NOT NULL, username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', image VARCHAR(255) NOT NULL, created_at DATETIME DEFAULT NULL, updated_at DATETIME DEFAULT NULL, is_active TINYINT(1) NOT NULL, is_verified TINYINT(1) NOT NULL, premium DATETIME DEFAULT NULL, stripe_id VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE reset_password_request (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, selector VARCHAR(20) NOT NULL, hashed_token VARCHAR(100) NOT NULL, requested_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', expires_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_7CE748AA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE model (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, age SMALLINT DEFAULT NULL, height SMALLINT DEFAULT NULL, country VARCHAR(255) DEFAULT NULL, city VARCHAR(255) DEFAULT NULL, image VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, slug VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE image (id INT AUTO_INCREMENT NOT NULL, image VARCHAR(255) NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE galery (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, thumbnail VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME DEFAULT NULL, slug VARCHAR(255) NOT NULL, is_free TINYINT(1) NOT NULL, cup_of_coffee INT NOT NULL, is_published TINYINT(1) NOT NULL, banner VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE picture (id INT AUTO_INCREMENT NOT NULL, galery_id INT NOT NULL, filename VARCHAR(255) NOT NULL, INDEX IDX_16DB4F89DA40A005 (galery_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE model_galery (model_id INT NOT NULL, galery_id INT NOT NULL, INDEX IDX_28BA77BF7975B7E7 (model_id), INDEX IDX_28BA77BFDA40A005 (galery_id), PRIMARY KEY(model_id, galery_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE galery_user (galery_id INT NOT NULL, user_id INT NOT NULL, INDEX IDX_4F5F4D32DA40A005 (galery_id), INDEX IDX_4F5F4D32A76ED395 (user_id), PRIMARY KEY(galery_id, user_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE contact (id INT AUTO_INCREMENT NOT NULL, firstname VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, subject VARCHAR(255) NOT NULL, message LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE comment (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, galery_id INT NOT NULL, message LONGTEXT NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME DEFAULT NULL, is_published TINYINT(1) NOT NULL, INDEX IDX_9474526CA76ED395 (user_id), INDEX IDX_9474526CDA40A005 (galery_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE comment_comment (comment_source INT NOT NULL, comment_target INT NOT NULL, INDEX IDX_6707307C95992761 (comment_source), INDEX IDX_6707307C8C7C77EE (comment_target), PRIMARY KEY(comment_source, comment_target)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE category_galery (category_id INT NOT NULL, galery_id INT NOT NULL, INDEX IDX_4B5995B12469DE2 (category_id), INDEX IDX_4B5995BDA40A005 (galery_id), PRIMARY KEY(category_id, galery_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE big_slider (id INT AUTO_INCREMENT NOT NULL, image VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE attachment (id INT AUTO_INCREMENT NOT NULL, galery_id INT NOT NULL, file_name VARCHAR(255) NOT NULL, file_size INT NOT NULL, created_at DATETIME NOT NULL, INDEX IDX_795FD9BBDA40A005 (galery_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE reset_password_request ADD CONSTRAINT FK_7CE748AA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE picture ADD CONSTRAINT FK_16DB4F89DA40A005 FOREIGN KEY (galery_id) REFERENCES galery (id)');
+        $this->addSql('ALTER TABLE model_galery ADD CONSTRAINT FK_28BA77BF7975B7E7 FOREIGN KEY (model_id) REFERENCES model (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE model_galery ADD CONSTRAINT FK_28BA77BFDA40A005 FOREIGN KEY (galery_id) REFERENCES galery (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE galery_user ADD CONSTRAINT FK_4F5F4D32DA40A005 FOREIGN KEY (galery_id) REFERENCES galery (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE galery_user ADD CONSTRAINT FK_4F5F4D32A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526CA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526CDA40A005 FOREIGN KEY (galery_id) REFERENCES galery (id)');
+        $this->addSql('ALTER TABLE comment_comment ADD CONSTRAINT FK_6707307C95992761 FOREIGN KEY (comment_source) REFERENCES comment (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE comment_comment ADD CONSTRAINT FK_6707307C8C7C77EE FOREIGN KEY (comment_target) REFERENCES comment (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE category_galery ADD CONSTRAINT FK_4B5995B12469DE2 FOREIGN KEY (category_id) REFERENCES category (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE category_galery ADD CONSTRAINT FK_4B5995BDA40A005 FOREIGN KEY (galery_id) REFERENCES galery (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE attachment ADD CONSTRAINT FK_795FD9BBDA40A005 FOREIGN KEY (galery_id) REFERENCES galery (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE category_galery DROP FOREIGN KEY FK_4B5995B12469DE2');
+        $this->addSql('ALTER TABLE comment_comment DROP FOREIGN KEY FK_6707307C95992761');
+        $this->addSql('ALTER TABLE comment_comment DROP FOREIGN KEY FK_6707307C8C7C77EE');
+        $this->addSql('ALTER TABLE attachment DROP FOREIGN KEY FK_795FD9BBDA40A005');
+        $this->addSql('ALTER TABLE category_galery DROP FOREIGN KEY FK_4B5995BDA40A005');
+        $this->addSql('ALTER TABLE comment DROP FOREIGN KEY FK_9474526CDA40A005');
+        $this->addSql('ALTER TABLE galery_user DROP FOREIGN KEY FK_4F5F4D32DA40A005');
+        $this->addSql('ALTER TABLE model_galery DROP FOREIGN KEY FK_28BA77BFDA40A005');
+        $this->addSql('ALTER TABLE picture DROP FOREIGN KEY FK_16DB4F89DA40A005');
+        $this->addSql('ALTER TABLE model_galery DROP FOREIGN KEY FK_28BA77BF7975B7E7');
+        $this->addSql('ALTER TABLE comment DROP FOREIGN KEY FK_9474526CA76ED395');
+        $this->addSql('ALTER TABLE galery_user DROP FOREIGN KEY FK_4F5F4D32A76ED395');
+        $this->addSql('ALTER TABLE reset_password_request DROP FOREIGN KEY FK_7CE748AA76ED395');
+        $this->addSql('DROP TABLE attachment');
+        $this->addSql('DROP TABLE big_slider');
+        $this->addSql('DROP TABLE category');
+        $this->addSql('DROP TABLE category_galery');
+        $this->addSql('DROP TABLE comment');
+        $this->addSql('DROP TABLE comment_comment');
+        $this->addSql('DROP TABLE contact');
+        $this->addSql('DROP TABLE galery');
+        $this->addSql('DROP TABLE galery_user');
+        $this->addSql('DROP TABLE image');
+        $this->addSql('DROP TABLE model');
+        $this->addSql('DROP TABLE model_galery');
+        $this->addSql('DROP TABLE picture');
+        $this->addSql('DROP TABLE reset_password_request');
+        $this->addSql('DROP TABLE user');
+    }
+}

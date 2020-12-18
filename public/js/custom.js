448d37f38440624038cb2334c4e5b5479f29ebeb
Bastien SOARES <bsp95@hotmail.fr>
2020-12-18 19:14:26 +0100

    Fix avatar display on user profil and navbar

--- public/js/custom.js
+++ public/js/custom.js
@@ -20,7 +20,7 @@ function readURL(input) {
         var reader = new FileReader();
 
         reader.onload = function (e) {
-            $('#avatar').attr('src', e.target.result);
+            $('#avatar').css("background-image", "url(" + e.target.result + ")");
         }
 
         reader.readAsDataURL(input.files[0]);

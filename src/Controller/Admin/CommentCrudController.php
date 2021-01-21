233a8e4b3771359e64a024242ddbc90c7908cfbb
Bastien SOARES <bsp95@hotmail.fr>
2021-01-21 12:26:32 +0100

    Disable new and edit action on payment CRUD

--- src/Controller/Admin/CommentCrudController.php
+++ src/Controller/Admin/CommentCrudController.php
@@ -4,7 +4,8 @@ namespace App\Controller\Admin;
 
 use App\Entity\Comment;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
-use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
@@ -19,6 +20,15 @@ class CommentCrudController extends AbstractCrudController
         return Comment::class;
     }
 
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            ->remove(Crud::PAGE_INDEX, Action::NEW)
+            ->remove(Crud::PAGE_INDEX, Action::EDIT)
+        ;
+    }
+
     public function configureCrud(Crud $crud): Crud
     {
         return $crud
@@ -27,13 +37,10 @@ class CommentCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
-        $mainPanel = FormField::addPanel('Main Informations')->setIcon('fas fa-edit')->addCssClass('col-md-8 float-left');
-        $advancedSettingsPanel = FormField::addPanel('Advanced Settings', 'fas fa-cogs')->addCssClass('col-md-4 float-right');
         $message = TextareaField::new('message');
         $user = AssociationField::new('user')->setFormTypeOptions(['disabled' => true]);
         $galery = AssociationField::new('galery')->setFormTypeOptions(['disabled' => true]);
         $replyTo = AssociationField::new('replyTo')->setFormTypeOptions(['disabled' => true]);
-        $comments = AssociationField::new('comments');
         $isPublished = BooleanField::new('isPublished');
         $createdAt = DateTimeField::new('createdAt')->setCustomOption('dateTimePattern', 'dd/MM/yyyy')->setFormTypeOptions(['disabled' => true]);
         $updatedAt = DateTimeField::new('updatedAt')->setCustomOption('dateTimePattern', 'dd/MM/yyyy')->setFormTypeOptions(['disabled' => true]);
@@ -43,8 +50,6 @@ class CommentCrudController extends AbstractCrudController
             return [$id, $user, $galery, $replyTo, $createdAt, $isPublished];
         } elseif (Crud::PAGE_DETAIL === $pageName) {
             return [$id, $message, $createdAt, $updatedAt, $isPublished, $user, $galery, $replyTo];
-        } elseif (Crud::PAGE_NEW === $pageName || Crud::PAGE_EDIT === $pageName) {
-            return [$mainPanel, $message, $user, $galery, $replyTo, $advancedSettingsPanel, $isPublished, $createdAt, $updatedAt];
         }
     }
 }
